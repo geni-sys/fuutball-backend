c64c94af9327936942a5a40a35937989dec8eb1b
elias alexandre <elliasallex@gmail.com>
2021-07-30 16:03:10 -0300

    feat: Tags provider singleton

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,12 @@
 import { container } from "tsyringe";
 
 import { GamesRepository } from "@modules/Game/infra/typeorm/repositories/GamesRepository";
+import { TagsRepository } from "@modules/Game/infra/typeorm/repositories/TagsRepository";
 import { IGamesRepository } from "@modules/Game/Repositories/IGamesRepository";
+import { ITagsRepository } from "@modules/Game/Repositories/ITagsRepository";
 
 container.registerSingleton<IGamesRepository>(
   "GamesRepository",
   GamesRepository
 );
+container.registerSingleton<ITagsRepository>("TagsRepository", TagsRepository);
